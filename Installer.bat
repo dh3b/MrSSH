40fcbbd6afb1ba7aa469aa3970bcd96f56b8ba77
dh3b <74708369+dh3b@users.noreply.github.com>
2021-10-06 21:00:58 +0200

    Update Installer.bat

--- Installer.bat
+++ Installer.bat
@@ -1,17 +1,3 @@
-echo Welcome to batch scripting!
-echo "Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
-
-Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
-
-Start-Service sshd
-
-Set-Service -Name sshd -StartupType 'Automatic'
-
-if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
-    Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
-    New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
-} else {
-    Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
-}">%temp%\ssh.ps1
+curl https://raw.githubusercontent.com/dh3b/MrSSH/main/SSH.ps1 -O ssh.ps1
 
 powershell ./ssh.ps1

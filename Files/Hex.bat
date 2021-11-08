11855c19c45c84bd16bcb7c1fe23d43096225d83
dheb <74708369+dh3b@users.noreply.github.com>
2021-11-08 18:35:47 +0100

    Hex coding and id webhook system

---
+++ Files/Hex.bat
@@ -0,0 +1,36 @@
+@echo on
+setlocal enabledelayedexpansion
+
+if /i "%~1"=="-plain" (
+    set "PlainString=%~2"
+    call:PlainToHex
+    exit /b
+)
+if /i "%~1"=="-hex" (
+    set "HexString=%~2"
+    call:HexToPlain
+    exit /b
+)
+exit /b
+
+:PlainToHex
+echo !PlainString!>PlainText.hex
+certutil -encodehex PlainText.hex output.hex >nul
+set "HexString="
+for /f "delims=" %%A in (output.hex) do (
+    set "line=%%A"
+    set "line=!line:~5,48!"
+    set "HexString=!HexString!!line!"
+)
+del PlainText.hex output.hex
+set "HexString=!HexString: =!"
+echo !HexString!
+goto:eof
+
+:HexToPlain
+echo !HexString!>HexString.hex
+certutil -decodehex HexString.hex output.hex >nul
+set /p PlainString=<output.hex
+del HexString.hex output.hex
+echo !PlainString!
+goto:eof

